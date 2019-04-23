6d43f2ce2eee9810457fd76ffb8a3b67a7cebff6
GatoReno <dmn@DMNs-MacBook-Pro.local>
2019-04-23 14:41:25 -0500

    primer commit0

---
+++ src/keys.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+'use strict';
+
+
+module.exports = {
+    database: {
+        host: 'us-cdbr-iron-east-02.cleardb.net',
+        user: 'b89790c716b125',
+        password: 'c3b01f29',
+        database: 'heroku_16c41959ed8b28c'
+    }
+}
