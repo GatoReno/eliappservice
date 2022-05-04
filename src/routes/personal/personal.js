9119638b0132a2859689e22dc3941dc66efce9a6
DESKTOP-SU7N3I6\USUARIO <andresagfa@outlook.com>
2022-05-04 18:13:24 -0500

    z

--- src/routes/personal/personal.js
+++ src/routes/personal/personal.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-const pool = require('../../dbdb');
+const pool = require('../../db');
 
 const {
     isLoggedIn,
